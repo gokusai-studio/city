8f5a1faf1381e521739efd5fee4cb90eec594a49
gokusai-studio <160147413+gokusai-studio@users.noreply.github.com>
2026-07-04 13:06:28 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'app/app.dart';
 import 'services/save/save_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SaveService.init();
-  await MobileAds.instance.initialize();
   runApp(const ProviderScope(child: HakataCitySimApp()));
 }
